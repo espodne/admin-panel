7faa359e123e4a7aba00de867ec80d20ae90fc1d
dima <dmitriikomarow@gmail.com>
2024-12-11 00:52:57 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { ColorModeContext, useMode } from "./styles/theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+
+import "./App.css";
+import Topbar from "./scenes/dashboard/global/Topbar";
+
+function App() {
+  const [theme, colorMode] = useMode();
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content">
+            <Topbar />
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+}
+
+export default App;
