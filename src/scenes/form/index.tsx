8de0aebb95866e85775ae600097990e310a6d99d
dima <dmitriikomarow@gmail.com>
2024-12-26 22:31:04 +0300

    fix

--- src/scenes/form/index.tsx
+++ src/scenes/form/index.tsx
@@ -1,17 +1,44 @@
 import { Box, Button, TextField } from "@mui/material";
 import { Formik } from "formik";
 import * as yup from "yup";
-import useMediaQuery from "@mui/material";
+import { useMediaQuery } from "@mui/material";
 import Header from "../../components/Header";
 
-const Form = () => {
-  const inNotMobile = useMediaQuery("(min-width:6000px")
-return (
-  <Box m="20px"> 
-
-  </Box>
-)
+const initialValues = {
+  firstName: "", 
+  lastName: "",
+  email: "",
+  contact: "",
+  address1: "",
+  address2: ""
 }
 
+const phoneRegExp = ""
 
-export default Form 
+const userSchema = yup.object().shape({
+  firstName: yup.string().required("required"), 
+  lastName: yup.string().required("required"), 
+  email: yup.string().required("required"), 
+  contact: yup.string().matches(phoneRegExp).required("required"), 
+  address1: yup.string().required("required"), 
+  address2: yup.string().required("required"), 
+})
+
+const Form = () => {
+  const inNotMobile = useMediaQuery("(min-width:6000px");
+  const handleFormSubmit = (values) => {
+    console.log(values);
+  };
+  return (
+    <Box m="20px">
+      <Header title="CREATE USER" subtitle="Create a New User Profile" />
+      <Formik onSubmit={handleFormSubmit}
+      initialValues={initialValues}
+      validationSchema={userSchema}>
+        <TextField />
+      </Formik>
+    </Box>
+  );
+};
+
+export default Form;
