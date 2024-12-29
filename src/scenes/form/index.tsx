387def6dbddc7f5de01b52d8280fde306aed6806
dima <dmitriikomarow@gmail.com>
2024-12-29 23:55:22 +0300

    add files

--- src/scenes/form/index.tsx
+++ src/scenes/form/index.tsx
@@ -1,28 +1,29 @@
-import { Box, Button, TextField } from "@mui/material";
+import { Box, TextField } from "@mui/material";
 import { Formik } from "formik";
 import * as yup from "yup";
 import { useMediaQuery } from "@mui/material";
 import Header from "../../components/Header";
 
 const initialValues = {
-  firstName: "", 
+  firstName: "",
   lastName: "",
   email: "",
   contact: "",
   address1: "",
-  address2: ""
-}
+  address2: "",
+};
 
-const phoneRegExp = ""
+const phoneRegExp =
+  /^(\+?\d{1,4}[\s-]?)?\(?\d{1,4}\)?[\s-]?\d{1,4}[\s-]?\d{1,9}$/;
 
 const userSchema = yup.object().shape({
-  firstName: yup.string().required("required"), 
-  lastName: yup.string().required("required"), 
-  email: yup.string().required("required"), 
-  contact: yup.string().matches(phoneRegExp).required("required"), 
-  address1: yup.string().required("required"), 
-  address2: yup.string().required("required"), 
-})
+  firstName: yup.string().required("required"),
+  lastName: yup.string().required("required"),
+  email: yup.string().email("invalid email").required("required"),
+  contact: yup.string().matches(phoneRegExp).required("required"),
+  address1: yup.string().required("required"),
+  address2: yup.string().required("required"),
+});
 
 const Form = () => {
   const inNotMobile = useMediaQuery("(min-width:6000px");
@@ -32,10 +33,25 @@ const Form = () => {
   return (
     <Box m="20px">
       <Header title="CREATE USER" subtitle="Create a New User Profile" />
-      <Formik onSubmit={handleFormSubmit}
-      initialValues={initialValues}
-      validationSchema={userSchema}>
-        <TextField />
+      <Formik
+        onSubmit={handleFormSubmit}
+        initialValues={initialValues}
+        validationSchema={userSchema}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleBlur,
+          handleChange,
+          handleSubmit,
+        }) => (
+          <form onSubmit={handleSubmit}>
+            <Box display="grid" gap="30px" gridTemplateColumns="repeat(4, minmax(0,1fr))">
+
+            </Box>
+          </form>
+        )}
       </Formik>
     </Box>
   );
