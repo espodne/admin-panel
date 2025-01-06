1de759575c22db50ceeb57a1f919bde582d26464
dima <dmitriikomarow@gmail.com>
2025-01-06 18:44:36 +0300

    add faq scene

--- src/scenes/faq/index.tsx
+++ src/scenes/faq/index.tsx
@@ -1,7 +1,82 @@
-import React from 'react'
+import { Box, Typography, useTheme } from "@mui/material";
+import Header from "../../components/Header";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { tokens } from "../../styles/theme";
 
-export default function Faq() {
+const Faq = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
   return (
-    <div>Faq</div>
-  )
-}
+    <Box m="20px">
+      <Header title="FAQ" subtitle="Frequently Asked Questions Page" />
+      <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            An Importan question
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque
+            voluptate dicta exercitationem, nobis optio asperiores mollitia iste
+            ea saepe ut cumque ipsum qui inventore, molestias in omnis vitae
+            assumenda minima.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+
+      <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            Another Importan question
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem
+            itaque quibusdam quo quod culpa tempora explicabo voluptatibus
+            placeat voluptate quos magni quas dolorum architecto obcaecati rem,
+            odio eveniet quae necessitatibus?
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+
+      <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            Some Random question
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe,
+            molestiae, optio repellat commodi ipsam dolorum accusantium illo
+            dolore, suscipit iste praesentium. Perspiciatis quisquam suscipit
+            quam eligendi quod minus est ex.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+
+      <Accordion defaultExpanded>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            Your Importan question
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt beatae
+            harum quod sed iusto praesentium delectus, nemo quisquam sint ipsam
+            iure, esse nesciunt doloribus asperiores, aliquam excepturi
+            accusamus ad aliquid.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+    </Box>
+  );
+};
+
+export default Faq;
