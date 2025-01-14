1c91b6c896311d63a1a9f416623246e0d78e6ed1
dima <dmitriikomarow@gmail.com>
2025-01-14 22:07:56 +0300

    progress bar

--- src/scenes/dashboard/index.tsx
+++ src/scenes/dashboard/index.tsx
@@ -1,12 +1,25 @@
-import { Box } from "@mui/material";
+import { Box, Button, IconButton, Typography, useTheme } from "@mui/material";
+import { tokens } from "../../styles/theme";
 import Header from "../../components/Header";
+import { mockTransactions } from "../../data/mockData";
+import DownloadDoneOutlined from "@mui/icons-material/DangerousOutlined";
+import EmailIcon from "@mui/icons-material/Email";
+import PointOfSaleIcon from "@mui/icons-material/PointOfSale";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import TrafficIcon from "@mui/icons-material/Traffic";
+import LineChart from "../../components/LineChart";
+import BarChart from "../../components/BarChart";
+import GeoChart from "../../components/GeoChart";
+import StatBox from "../../components/StatBox";
+import ProgressCircle from "../../components/ProgressCircle";
+
 
 const Dashboard = () => {
   return (
     <Box m="20px">
-     <Box display="flex" justifyContent="space-between" alignItems="center"> 
-     <Header title="DASHBOARD" subtitle="Welcome to your Dashboard" />
-     </Box>
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <Header title="DASHBOARD" subtitle="Welcome to your Dashboard" />
+      </Box>
     </Box>
   );
 };
